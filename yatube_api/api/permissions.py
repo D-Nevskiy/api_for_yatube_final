53cbe1f3166ce95c1532196870a44c2513ee27ec
Danil Raspopov <anarant91@gmail.com>
2022-11-20 21:57:10 +0300

    ver 1.0

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,7 +5,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if hasattr(obj, 'author'):
-            return obj.author == request.user \
-                   or request.method in permissions.SAFE_METHODS
+            return obj.author == request.user or request.method\
+                   in permissions.SAFE_METHODS
         else:
             return request.method in permissions.SAFE_METHODS
